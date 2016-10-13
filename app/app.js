fff1dba08f84ac7b2065e33b02e455032bab4562
joakimgithub <joakim.gunnarsson@sogeti.com>
2016-10-13 13:43:09 +0200

    Added customerView

--- app/app.js
+++ app/app.js
@@ -3,13 +3,13 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
     'ngRoute',
-    'myApp.allCustomersView',
+    'myApp.customerView',
     'myApp.allCsisForCustomerView',
     'myApp.csiView'
 ])
 .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/allCustomersView'});
+  $routeProvider.otherwise({redirectTo: '/customerView'});
 }]);
 
 //app.constant('global.url', 'http://a01c01263c/CSIService/api/');
